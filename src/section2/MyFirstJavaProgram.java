53e38f4ad8fd1de4eac991b31f9b83ff6d478f59
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-26 14:55:16 -0700

    Day2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,20 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
+		System.out.println("Hello World");
 	
+		Robot r2d2 = new Robot();
 		
+		r2d2.miniaturize();
+		r2d2.penDown();
+		r2d2.setSpeed(50);
+		r2d2.setPenWidth(5);
+		
+		for (int i = 0; i < 4; i++){
+			r2d2.move(100);
+			r2d2.turn(90);
+			r2d2.setRandomPenColor();
+		}
 		
 	}
 }
--- src/section2/StarShow.java
+++ src/section2/StarShow.java
@@ -17,7 +17,7 @@ public class StarShow {
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+int =()
 		// 7. Make a variable to hold the Y position of the Robot and set it to 600
 
 		// 8. Make a variable to hold the star size and set it to 25
@@ -46,15 +46,17 @@ public class StarShow {
 
 	private void drawStar(int starSize) {
 		// 2. Put the robot's pen down
-
+robot.penDown();
 		// 4. Repeat both commands 5 times. See Figure 1 at http://bit.ly/star-show
+for (int i = 0; i < 5; i++) {
+	
 
 			// 1. Move the robot the distance of the starSize variable
-	
+	robot.move(50);
 			// 3. Turn the robot 144 degrees
-			
+			robot.turn(144);
 	}
-	
+	}	
 	public static void main(String[] args) {
 		new StarShow().makeStars();
 	}
